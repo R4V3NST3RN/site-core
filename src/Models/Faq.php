fff5f448d65a0c13ea5ce6aa7255cecc173fa780
R4V3NST3RN <janca.vaclav@gmail.com>
2026-08-09 09:07:49 +0200

    add slug to Faq as stable identifier for seeding

--- src/Models/Faq.php
+++ src/Models/Faq.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Faq extends Model
 {
@@ -11,10 +12,21 @@ class Faq extends Model
 
     protected $fillable = [
         'question',
+        'slug',
         'answer',
         'is_active',
         'order',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($faq) {
+            if (empty($faq->slug)) {
+                $faq->slug = Str::slug($faq->question);
+            }
+        });
+    }
 
     protected $casts = [
         'is_active' => 'boolean',
